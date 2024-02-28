ce30060de506dc8f5f7eec4a1cbb2cfac9238f45
Jose Rodriguez <131327988+RodriguezJose92@users.noreply.github.com>
2024-02-27 22:55:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -171,6 +171,6 @@ async function mudiExperience({skuNumber,fatherContainer}){
 const verify = new URLSearchParams(window.location.search).get('muditest')
 
 verify && mudiExperience({
-    skuNumber:"WEM7643CSIS0_MabeMex",
+    skuNumber:document.body.querySelector('.code').innerHTML,
     fatherContainer: document.body.querySelectorAll(`.image-gallery`)
 });
